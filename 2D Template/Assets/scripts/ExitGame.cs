a49cc481dbf231331eb59beb7ff5729e6dcc6b21
MUKILTEO\1725489 <kayla.gerlach42@gmail.com>
2025-09-24 10:13:32 -0700

    finished

--- 2D Template/Assets/scripts/ExitGame.cs
+++ 2D Template/Assets/scripts/ExitGame.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class Exit : MonoBehaviour
-{ 
+public class ExitGame : MonoBehaviour
+{
     // Update is called once per frame 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            SceneManager.LoadScene("startScreen");
+            Application.Quit();
 
 #if UNITY_EDITOR
-            UnityEditor.EditorApplication.ExitPlaymode(); 
-#endif 
+            UnityEditor.EditorApplication.ExitPlaymode();
+#endif
         }
-    } 
-} 
+    }
+}
